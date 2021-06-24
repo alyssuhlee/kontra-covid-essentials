64fd7096d354cf12345cc536db39d5f7374ab003
Alyssa Alexandra Lee <76801500+aslee2@users.noreply.github.com>
2021-06-24 18:31:37 +0800

    Delete Sprint 3/Backend Programming & Database/mysite directory

--- Sprint 3/Backend Programming & Database/mysite/main/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Post
-
-class Post(forms.Post):
-    class Meta(object):
-        model = Post
-        fields = ("name", "email", "phone", "query")
--- Sprint 3/Backend Programming & Database/mysite/main/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Post(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    phone = models.CharField(max_length=100)
-    query = models.CharField(max_length=500) 
